0db0ba35ef24d1c955eca4171f35b5bc86cca09d
farros <31735132+farrosmufid@users.noreply.github.com>
2025-06-23 17:42:09 +1000

    Removed unecessary files

--- StreamingTypes.ts
+++
@@ -1,6 +0,0 @@
-// Add source_language to ServerTextData interface
-export interface ServerTextData extends ServerTranslationDataBase {
-    event: 'translation_text';
-    payload: string;
-    source_language?: string;  // 💾 ADD SOURCE LANGUAGE
-  }
